567bef68663f36cf518e5eda92af199a9ef3a377
szavgorodni <szavgorodni@hotmail.com>
2017-08-03 14:14:21 -0700

    save to file added

--- build.sbt
+++ build.sbt
@@ -4,14 +4,22 @@ version := "1.0"
 
 scalaVersion := "2.11.11"
 
+val sparkVersion = "2.2.2"
+
+resolvers ++= Seq(
+
+  "apache-snapshots" at "http://repository.apache.org/snapshots/"
+
+)
+
 libraryDependencies ++= Seq(
-  "org.apache.spark" %% "spark-core" % "2.2.0",
-  "org.apache.spark" %% "spark-sql" % "2.2.0",
+  "org.apache.spark" %% "spark-core" % sparkVersion exclude("jline", "2.12"),
+  "org.apache.spark" %% "spark-sql" % sparkVersion,
   "org.apache.hadoop" % "hadoop-common" % "2.7.0" exclude ("org.apache.hadoop","hadoop-yarn-server-web-proxy"),
-  "org.apache.spark" %% "spark-hive" % "2.2.0" exclude ("org.apache.hadoop","hadoop-yarn-server-web-proxy"),
-  "org.apache.spark" %% "spark-yarn" % "2.2.0" exclude ("org.apache.hadoop","hadoop-yarn-server-web-proxy"),
-  "org.apache.spark" %% "spark-mllib" % "2.2.0",
-  "org.apache.spark" %% "spark-streaming" % "2.2.0",
-  "org.apache.spark" %% "spark-graphx" % "2.2.0",
-  "org.apache.spark" %% "spark-core" % "2.2.0"
+  "org.apache.spark" %% "spark-hive" % sparkVersion,
+  "org.apache.spark" %% "spark-yarn" % sparkVersion,
+  "org.apache.spark" %% "spark-mllib" % sparkVersion,
+  "org.apache.spark" %% "spark-streaming" % sparkVersion,
+  "org.apache.spark" %% "spark-graphx" % sparkVersion,
+  "org.apache.spark" %% "spark-core" % sparkVersion"
 )
--- src/main/scala/DSScore.scala
+++ src/main/scala/DSScore.scala
@@ -1,13 +1,44 @@
-import WordCount.{sc, spark_master}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.{Dataset, SparkSession}
+import java.sql.Date
 
+import java.io.File
+import org.apache.commons.io._
 
-// command line params:
-// menu:Run:EditConfigurations:Program Arguments
-// local[*] E:\projects\data\global_superstore_2016.txt
-
+// refer to data types here:
+//https://gist.github.com/yoyama/ce83f688717719fc8ca145c3b3ff43fd
+// https://spark.apache.org/docs/latest/sql-programming-guide.html#dataframegroupby-retains-grouping-columns
 object DSScore {
+
+  // name of class attributes have to be exactly same as as header in the file
+  // otherwise the file will not be parsed
+  // NB: import java.sql.Date for correct date type parsing
+  case class SStore(
+    row_id:Int,
+    order_id:String,
+    order_date:Date,
+    ship_date:Date,
+    ship_mode:String,
+    customer_id:String,
+    customer_name:String,
+    segment:String,
+    postal_code:String,
+    city:String,
+    state:String,
+    country:String,
+    region:String,
+    market:String,
+    product_id:String,
+    category:String,
+    sub_category:String,
+    product_name:String,
+    sales:Double,
+    quantity:Int,
+    discount:Double,
+    profit:Double,
+    shipping_cost:Double,
+    order_priority:String
+  )
 
   var sc: SparkContext = null
   //var ss: SparkSession = null
@@ -44,11 +75,18 @@ object DSScore {
     // disable excessive logging
     sc.setLogLevel("WARN")
 
-    val ss = SparkSession.builder
+    val ss :SparkSession = SparkSession.builder
       .master(spark_master)
       .enableHiveSupport() // hive dependency added to build.sbt
+      // already created in E:\projects\spark\s\sbt_test\spark-warehouse
+      //.config("spark.sql.warehouse.dir", "E:\\projects\\spark\\s\\sbt_test\\target\\spark-warehouse")
       .appName(app_name)
       .getOrCreate()
+
+    // provides object encoder/serializer  for primitive data types
+    // NB:! imported from object - not class
+    import ss.implicits._
+
 
     // https://github.com/databricks/spark-csv
     // A library for parsing and querying CSV data with Apache Spark, for Spark SQL and DataFrames.
@@ -56,19 +94,95 @@ object DSScore {
     // date formats descibed here:
     // https://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html
 
-    val ds_score = ss
+    // data frame
+    val df_score = ss
       .read
-      .format("com.databricks.spark.csv")
+      // use .format() if .load(my_file) is used : i.e. format is not specified
+      // and .load(inputFile) is used instead of csv()
+      //.format("com.databricks.spark.csv")
       .option("header", "true") // Use first line of all files as header
       .option("delimiter", "\t")
       .option("dateFormat","MM/dd/yyyy")
-      .option("inferSchema","true")
-      .load(inputFile)
+      .option("inferSchema","true") // makes spark to make two passes on data: TODO: check how to avoid it
+      .csv(inputFile)
+    // .load(inputFile) //generic loader // have to specify format in that case
+
+    // data set
+    val ds_score = ss
+      .read
+      // use .format() if .load(my_file) is used : i.e. format is not specified
+      // and .load(inputFile) is used instead of csv()
+      //.format("com.databricks.spark.csv")
+      .option("header", "true") // Use first line of all files as header
+      .option("delimiter", "\t")
+      .option("dateFormat","MM/dd/yyyy")
+      .option("inferSchema","true") // makes spark to make two passes on data: TODO: check how to avoid it
+      .csv(inputFile)
+      // .load(inputFile) //generic loader // have to specify format in that case
+      .as[SStore]
+
+    // referring to ds is different than referring to df
+    val selectedDF = df_score.select("order_id")
+
+    // more object oriented way to query the data set
+    val selectedDS = ds_score.map(_.order_id)
+
+    // output execution plan
+    // shows that data set is mapped to the data types and data frame is not typed
+    println(selectedDF.queryExecution.optimizedPlan.numberedTreeString)
+    println(selectedDS.queryExecution.optimizedPlan.numberedTreeString)
 
     val  ds_score_count = ds_score.count()
-    ds_score.show()
+
+    // just output the sample in tab format
+    ds_score.show(10)
+
     println(ds_score_count)
-   // ds_score_count.show()
+
+    val ds_score_vw = ds_score.createOrReplaceTempView("ds_score_vw")
+    val hql_country ="'United States'"
+    val ds_score_group = ss.sql(
+      s"""
+         |SELECT
+         |   t.state
+         | , t.city
+         | , t.orders
+         | , (1 / (1 + exp(-1 * t.orders)))*2-1 as squash_score_orders -- 	(1 / (1 + exp(-x)))*2-1; alternative: tanh
+         | , tanh(t.orders) as tanh_score_orders -- alternative: tanh
+         | , MAX(t.orders) OVER() as max_orders
+         | , FLOOR((t.orders / MAX(t.orders) OVER())*10) as score_orders
+         | from (
+         |SELECT
+         |  state
+         | , city
+         | , sum(quantity) as orders
+         |FROM ds_score_vw
+         |WHERE country = $hql_country
+         |GROUP BY state, city
+         | ) t
+         |ORDER BY t.state, t.city
+      """.stripMargin // do not use () -- not sure why
+    )
+
+
+    val output_directory = "E:\\projects\\spark\\s\\sbt_test\\output\\ds_score_group"
+    FileUtils.deleteDirectory(new File(output_directory))
+
+    // save output
+    ds_score_group
+      .repartition(1) // save everything to one file
+      .write
+      .format("com.databricks.spark.csv")
+      .option("header","true")
+      .option("delimiter", "\t")
+      .save(output_directory)
+
+    ds_score_group.show(10)
+
+    // temp tables created for work with spark SQL
+    ss.catalog.listTables.show()
+
+    ss.stop()
 
   }
 }
